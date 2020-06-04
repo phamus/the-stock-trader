041cf2ef366fe00b563ddd8ff2aa884fcc6dfa98
phamus <sphamus@gmail.com>
2020-06-04 23:51:00 +0100

    Nav Bar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="container">
-    <router-view></router-view>
+    <app-header></app-header>
+    <div class="row">
+      <div class="col-md-12">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Header from "./components/Header.vue";
+export default {
+  components: {
+    appHeader: Header,
+  },
+};
 </script>
 
-<style></style>
+<style>
+body {
+  padding: 30px;
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,62 @@
 <template>
-  <h1>The Header Components</h1>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <router-link
+          to="/portfolio"
+          activeClass="active"
+          tag="li"
+          class="nav-item"
+          ><a class="nav-link"> Portfolio</a></router-link
+        >
+        <router-link to="/stocks" activeClass="active" tag="li" class="nav-item"
+          ><a class="nav-link"> Stocks</a></router-link
+        >
+      </ul>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item">
+          <a class="nav-link" href="#">End Day</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Save & Load
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Save Data</a>
+            <a class="dropdown-item" href="#">Load Data</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link disabled"
+            href="#"
+            tabindex="-1"
+            aria-disabled="true"
+            >Disabled</a
+          >
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
